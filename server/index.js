1f58f5b396a0c148cb6406a729a683c1326a6fbc
harikesh <138229790+harik3shhh@users.noreply.github.com>
2023-12-29 17:39:05 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,6 +15,13 @@ const corsOptions={
     credentials: true,
 }
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://toast-message-b.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 //cors
 app.use(cors(corsOptions));
 
