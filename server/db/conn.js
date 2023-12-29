09d32a54c0f4b41c2fc5d2ebf2a50aa1d1471848
harikesh <138229790+harik3shhh@users.noreply.github.com>
2023-12-29 17:07:04 +0530

    Update conn.js

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-DB = mongodb+srv://harikeshlinux:t5tKUIP0SoaULLAC@cluster0.6ehpqhq.mongodb.net/toastmessage?retryWrites=true&w=majority;
+DB = "mongodb+srv://harikeshlinux:t5tKUIP0SoaULLAC@cluster0.6ehpqhq.mongodb.net/toastmessage?retryWrites=true&w=majority";
 
 
 mongoose.connect(DB).then(()=>{
