d40ad32b40d0cfe016baf6fe48a564608017fdda
harikesh <138229790+harik3shhh@users.noreply.github.com>
2023-12-29 17:19:20 +0530

    Update Message.jsx

--- client/src/pages/Message.jsx
+++ client/src/pages/Message.jsx
@@ -35,8 +35,7 @@ const handleSubmit = async(e)=>{
     console.log(user);
 
     try {
-        const response = await fetch("toast-message-eight.vercel.app
-+2/api/form/message", {
+        const response = await fetch("toast-message-eight.vercel.app+2/api/form/message", {
             method: "POST",
             headers:{
                 "Content-Type":"application/json",
